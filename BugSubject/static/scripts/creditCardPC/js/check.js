6ba820637e72279357c97be9aa2a3b1721c5e927
s512836521 <61528704+s512836521@users.noreply.github.com>
2020-03-06 17:01:18 +0800

    Add files via upload

---
+++ BugSubject/static/scripts/creditCardPC/js/check.js
@@ -0,0 +1,387 @@
+var AREA_CODE={11:"北京",12:"天津",13:"河北",14:"山西",15:"内蒙古",21:"辽宁",22:"吉林",23:"黑龙江 ",31:"上海",32:"江苏",33:"浙江",34:"安徽",35:"福建",36:"江西",37:"山东",41:"河南",42:"湖北 ",43:"湖南",44:"广东",45:"广西",46:"海南",50:"重庆",51:"四川",52:"贵州",53:"云南",54:"西藏 ",61:"陕西",62:"甘肃",63:"青海",64:"宁夏",65:"新疆",71:"台湾",81:"香港",82:"澳门",91:"国外 "};
+var DATE_FORMAT = "yyyymmdd";
+var REGEXP_FORMAT = new RegExp(/(yyyy|mm|dd|hh|mi|ss|ms)/gi);
+var REGEXP_INTEGER  = new RegExp(/^[0-9]+$/);
+
+//身份证校验
+function isIDNumber(s){
+
+    //检查长度
+    switch(s.length){
+        case 15: case 18:{
+            break;
+        }
+        default:{
+            return false;
+        }
+    }
+
+    //检查是否为数字
+    var testInt = (s.length == 15) ? s : s.substr(0,17);
+    if(!REGEXP_INTEGER.test(testInt)){
+        return false;
+    }
+
+    //检查区域代码
+    var areaCode = parseInt(s.substr(0,2));
+    if(!AREA_CODE[areaCode]){
+        return false;
+    }
+
+    //检查生日是否合法
+    var birthDay = (s.length == 15) ? ("19" + s.substr(6,6)) : s.substr(6,8);
+    if(!isDate(birthDay,DATE_FORMAT)){
+        return false;
+    }
+
+    //检查校验位是否合法
+    if(s.length == 18){
+        var testNumber = (parseInt(s.charAt(0))+parseInt(s.charAt(10))) * 7
+            + (parseInt(s.charAt(1))+parseInt(s.charAt(11))) * 9
+            + (parseInt(s.charAt(2))+parseInt(s.charAt(12))) * 10
+            + (parseInt(s.charAt(3))+parseInt(s.charAt(13))) * 5
+            + (parseInt(s.charAt(4))+parseInt(s.charAt(14))) * 8
+            + (parseInt(s.charAt(5))+parseInt(s.charAt(15))) * 4
+            + (parseInt(s.charAt(6))+parseInt(s.charAt(16))) * 2
+            + (parseInt(s.charAt(7))) * 1
+            + (parseInt(s.charAt(8))) * 6
+            + (parseInt(s.charAt(9))) * 3;
+        
+        if(s.charAt(17)!="10x98765432".charAt(testNumber % 11) ){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isDate(dateString,format){
+    var year,month,day;
+    format = format || DATE_FORMAT;
+    var matchArray = format.match(REGEXP_FORMAT);
+    for(var i = 0;i<matchArray.length;i++){
+        var postion = format.indexOf(matchArray[i]);
+        switch(matchArray[i]){
+            case "yyyy":{
+                var yyyyStr = dateString.substr(postion,4);
+                if(!REGEXP_INTEGER.test(yyyyStr)){
+                    return false;
+                }
+                year = parseInt(yyyyStr,10);
+                break;
+            }
+            case "mm":{
+                var mmStr = dateString.substr(postion,2);
+                if(!REGEXP_INTEGER.test(mmStr)){
+                    return false;
+                }
+                month = parseInt(mmStr,10) - 1;
+                break;
+            }
+            case "dd":{
+                var ddStr = dateString.substr(postion,2);
+                if(!REGEXP_INTEGER.test(ddStr)){
+                    return false;
+                }
+                day = parseInt(ddStr,10);
+                break;
+            }
+        }
+    }
+    var dateTest = new Date(year,month,day);
+    var testYear = dateTest.getFullYear();
+    var testMonth = dateTest.getMonth();
+    var testDay = dateTest.getDate();
+    return (year == testYear && month == testMonth && day == testDay)
+}
+
+//性别、生日页面展示
+function show_Sex_Birthday(s){
+    var birthday = "";
+    if(s.length == 18){
+        birthday = s.substring(6,14);
+        if(s.charAt(s.length-2)%2 == 0){
+            var sex = "女";
+        }else{
+            var sex = "男";
+        }
+    }else{
+        birthday = "19"+s.substring(6,12);
+        if(s.charAt(s.length-1)%2 == 0){
+            var sex = "女";
+        }else{
+            var sex = "男";
+        }
+    }
+    var showPresonInfo =  " 性别："+ sex + "   生日："+ birthday.substring(0,4) + "年" + birthday.substring(4,6)  +"月" + birthday.substring(6,8) + "日";
+    return showPresonInfo;
+}
+
+//获取性别
+function getSex(s){
+    var birthday = "";
+    if(s.length == 18){
+        birthday = s.substring(6,14);
+        if(s.charAt(s.length-2)%2 == 0){
+            var sex = "1";
+        }else{
+            var sex = "0";
+        }
+    }else{
+        birthday = "19"+s.substring(6,12);
+        if(s.charAt(s.length-1)%2 == 0){
+            var sex = "1";
+        }else{
+            var sex = "0";
+        }
+    }
+    return sex;
+}
+
+//获取生日
+function getBirthday(s){
+    var birthday = "";
+    if(s.length == 18){
+        birthday = s.substring(6,14);
+        if(s.charAt(s.length-2)%2 == 0){
+            var sex = "女";
+        }else{
+            var sex = "男";
+        }
+    }else{
+        birthday = "19"+s.substring(6,12);
+        if(s.charAt(s.length-1)%2 == 0){
+            var sex = "女";
+        }else{
+            var sex = "男";
+        }
+    }
+    return birthday.substring(0,4) + birthday.substring(4,6)  + birthday.substring(6,8);
+}
+
+//时间个位数转换2位
+function pad2(n){
+    return (n < 10 ? '0' + n : n);
+}
+
+//获取当前时间
+function nowTime(){
+    var nowtime = new Date();
+    return nowtime.getFullYear().toString() + pad2(nowtime.getMonth()+1) + pad2(nowtime.getDate()) + pad2(nowtime.getHours()) + pad2(nowtime.getMinutes()) + pad2(nowtime.getSeconds());
+}
+
+/**
+ * 卡规则判断 2011年7月26日修改by zzc, 增加非1或685800开头的账号判断，如果不符合卡规则，则直接判断账号输入错误（e）
+ * 返回一个对象，对象有卡归属和卡类别两个属性，如下： { cardOwner : "0", //卡归属： “0” 广东发展银行，“s”
+ * 可能为广发折对私账号，“g”可能为广发对公账户，“x” 未知 cardType : "0" //卡类别： “0” 借记卡， “1” 信用卡对私账号,
+ * “2”信用卡对公账号， “x” 未知, “e” 账号错误 }
+ */
+function judgeAccRole2(accNo){
+	var rule = {
+        cardOwner : "x",
+        cardType : "x"
+	};
+	if(isString(accNo)){
+		// 借记卡bin
+		var bin1 = [{bin:"622568",len:"19"}, {bin:"9111",len:"19"}, {bin:"685800",len:"18"}, {bin:"685800",len:"19"},{bin:"621462",len:"19"}];
+		// 信用卡bin
+		var bin2 = [ 
+					"406365", "406366", 
+					"428911",
+					"436768", "436769","436770", "436771", 
+					"487013",
+					"491032", "491033", "491034", "491035", "491036", "491037", "491038", "493427", 
+					"518364", 
+					"520152", "520382", "523961", "523966", "528931", 
+					"541709", "541710", "548844", 
+					"552794", "558894",		           		          		          
+					"622555", "622556", "622557", "622558", "622559", "622560", "625071", "625072", "625073", "628259", "628260",		            
+					// 未发卡
+					"520152", "520382", "622555", "622556"
+					];
+		// 存折或对公账户
+		var bin3 = [{bin:"1",len:"18"}];
+		
+		var accPrefix1 = accNo.substring(0,6);
+		var accPrefix2 = accNo.substring(0,4);
+		var accPrefix3 = accNo.substring(0,1);
+		var accPrefix4 = accNo.substring(6,9);
+		
+		// 判断是否为广发借记卡
+		for( var i = 0, len = bin1.length; i < len; i++ ){
+			if( (accPrefix1 == bin1[i].bin || accPrefix2 == bin1[i].bin) 
+					&& (accNo.length == parseInt(bin1[i].len)) ) {
+				rule.cardOwner = "0";
+				rule.cardType = "0";
+
+				return rule;
+			}else if(accPrefix1 != '685800' && (accPrefix1 == bin1[i].bin || accPrefix2 == bin1[i].bin)&& (accNo.length != parseInt(bin1[i].len))){
+				rule.cardOwner = "0";
+				rule.cardType = "e";
+
+				return rule;
+			}
+		}
+		// 判断是否为广发信用卡
+		for( var i = 0, len = bin2.length; i < len; i++ ){
+			if( accPrefix1 == bin2[i] && accNo.length == 16 ){
+				rule.cardOwner = "0";
+				if(accNo.charAt(6) == '0'){
+					rule.cardType = "2";
+				}else{
+					rule.cardType = "1";
+				}
+				return rule;
+			}else if(accPrefix1 == bin2[i] && accNo.length != 16){
+					rule.cardOwner = "0";
+					rule.cardType = "e";
+				return rule;
+			}
+		}
+		// 判断是否为广发折或者对公账户
+		for( var i = 0, len = bin3.length; i < len; i++ ){
+			if( accPrefix3 == bin3[i].bin  && (accNo.length == parseInt(bin3[i].len)) ) {
+				if((500*1<=accPrefix4*1)&&(accPrefix4*1<=900*1)){
+					rule.cardOwner = "g";
+				}else{
+					rule.cardOwner = "s";
+				}
+				rule.cardType = "x";
+			}
+				return rule;
+		}
+	}
+	return rule;
+}
+
+
+/**
+ * 验证还款设置 个人结算账户（本行）
+*/
+function checkAutoPayAcc(accNo){
+	var accountNo = accNo;
+	if(accountNo){
+		if(!(new RegExp(/^[0-9]{1,19}$/).test(accountNo))){
+			return false;
+		}
+		if(startsWith(accountNo,"1")||startsWith(accountNo,"6")){
+			if(accountNo.length > 10){
+				var num1 = accountNo.substring(8,10);
+				if(parseInt(num1) >= 21 && parseInt(num1) <= 60){
+					var ret = checkAccountNo(accountNo);
+					if(ret){
+						return true;
+					}else{
+						
+						return false;
+					}
+				}else{
+					
+					return false;
+				}
+			}else{
+				
+				return false;
+			}
+			
+		}else if(startsWith(accountNo,"9")){
+			if(accountNo.length > 6){
+				var num2 = accountNo.substring(4,6);
+				if(parseInt(num2) >= 21 && parseInt(num2) <= 60){
+					var ret = checkAccountNo(accountNo);
+					if(ret){
+						return true;
+					}else{
+						
+						return false;
+					}
+				}else{
+					
+					return false;
+				}
+			}else{
+				
+				return false;
+			}
+			
+		}else{
+			//判断是否为本行借记卡
+			var ret = checkAccountNo(accountNo);
+			if(ret){
+				return true;
+			}else{
+				
+				return false;
+			}
+		}
+	}
+}
+
+
+/**
+* 对还款设置中他行账户进行验证（跨行）
+* */
+function checkrRecAccount(accNo){
+    var debitCardNo = accNo;//借记卡账号
+    if((new RegExp(/^[0-9]{1,7}$/).test(debitCardNo))){
+        return false;
+    }
+    if(!(new RegExp(/^[0-9]{8,25}$/).test(debitCardNo))){
+        return false;
+    }
+    
+    var cardInfo = judgeAccRole2(debitCardNo);
+    var bankType = cardInfo.cardOwner;
+    var cardType = cardInfo.cardType;
+    if(((bankType=='0'||bankType=='s'||bankType=='g') && cardType=='0') || debitCardNo.length<11 || debitCardNo.length>25){
+        return false;
+    }
+    
+    return true;
+}
+
+//卡宾校验
+function startsWith(obj,str){
+
+    if(str == null||str==""||this.length==0||str.length>obj.length){
+        return false;
+    }
+
+    if(obj.substr(0,str.length) == str){
+        return true;
+    }else{
+        return false;
+    }
+
+    return true;
+}
+
+//卡宾校验
+function checkAccountNo(accountNo){
+    var params = judgeAccRole2(accountNo);
+
+    if(params.cardOwner == 'o'||params.cardOwner == "s"||params.cardOwner=="g" && params.cardType == "0"){
+       return true; 
+    }else{
+       return false;
+    }
+}
+
+function isString(str){
+    return typeof str == 'string' && str.constructor == String;
+}
+
+//字段校验（公共方法）
+function checkString(){
+
+}
+
+// function isChinese(val){
+//     let reg = new RegExp("^[\u4e00-\u9fa5]+$");
+//     return reg.test(val);
+// }
+
+// function isChiEngNum(val){
+//     let reg = new RegExp("^[\u4e00-\u9fa5|A-Za-z|0-9]+$");
+//     return reg.test(val);
+// }
---
+++ BugSubject/static/scripts/creditCardPC/js/showCardInfo.js
@@ -0,0 +1,445 @@
+//one卡特色卡板字段
+var oneCardType = "";
+
+
+//卡品牌展示
+//卡板、选中展示、
+function showCardBrand (cardId,cardBrand,List){
+
+    var cardVersionInfoList = List;
+    //选中初始化
+    var cardC = document.getElementById('cardC');
+    var cardM = document.getElementById('cardM');
+    var cardV = document.getElementById('cardV');
+    var cardCM = document.getElementById('cardCM');
+    var cardCV = document.getElementById('cardCV');
+
+    cardC.className = "i_nomal";
+    cardM.className = "i_nomal";
+    cardV.className = "i_nomal";
+    cardCM.className = "i_nomal";
+    cardCV.className = "i_nomal";
+
+    //卡类选中
+    var cardP = document.getElementById('cardP');
+    var cardJ = document.getElementById('cardJ');
+    var cardB = document.getElementById('cardB');
+    var cardT = document.getElementById('cardT');
+
+    cardP.className = "i_nomal";
+    cardJ.className = "i_nomal";
+    cardB.className = "i_nomal";
+    cardT.className = "i_nomal";
+
+    //卡别选中初始化
+    var cardZ = document.getElementById('cardZ');
+    var cardZF = document.getElementById('cardZF');
+
+    cardZ.className = "i_nomal";
+    cardZF.className = "i_nomal";
+
+    //清除选中 one卡
+    // if(cardId != "1042"){ 
+    //     for(var i=0;i<cardVersionInfoList.length;i++){
+    //         var kcoll = cardVersionInfoList[i];
+    
+    //         var liId = document.getElementById(kcoll.cardVersionId);
+    //         liId.className = "";
+    
+    //     }
+    // }
+
+    //选中
+    var ui = document.getElementById(cardBrand);
+    ui.className = "i_nomal i_sel";
+
+    //卡板值
+    var cardBandType;
+    if(cardBrand == "cardC"){
+        cardBandType = "C";
+    }else if(cardBrand == "cardM"){
+        cardBandType = "M";
+    }else if(cardBrand == "cardV"){
+        cardBandType = "V";
+    }else if(cardBrand == "cardCM"){
+        cardBandType = "C";
+    }else if(cardBrand == "cardCV"){
+        cardBandType = "C";
+    }
+    
+    //卡类、卡别展示
+    analysiscardVersionList(cardBandType,cardVersionInfoList);
+    
+    return cardBandType;
+}
+
+//卡类、卡别显示
+function analysiscardVersionList(cardBandType,cardVersionInfoList){
+
+    //卡类
+    var cardP = document.getElementById('cardP');
+    var cardJ = document.getElementById('cardJ');
+    var cardB = document.getElementById('cardB');
+    var cardT = document.getElementById('cardT');
+
+    cardP.style.display = "none";
+    cardJ.style.display = "none";
+    cardB.style.display = "none";
+    cardT.style.display = "none";
+
+    //卡别
+    var cardZ = document.getElementById('cardZ');
+    var cardZF = document.getElementById('cardZF');
+
+    cardZ.style.display = "none";
+    cardZF.style.display = "none";
+
+    for(i=0 ; i < cardVersionInfoList.length ; i++){
+        var kcoll = cardVersionInfoList[i];
+
+        if(kcoll.cardBrand == cardBandType){
+            //展示卡类型
+            if(kcoll.cardClass == 'P'){
+                cardP.style.display = "";
+            }else if(kcoll.cardClass == 'J'){
+                cardJ.style.display = "";
+            }else if(kcoll.cardClass == 'B'){
+                cardB.style.display = "";
+            }else if(kcoll.cardClass == 'T'){
+                cardT.style.display = "";
+            }
+            
+        }
+    }
+
+}
+
+//卡类选中
+function changeCardLevel(cardId, cardBrand, cardClass, cardVersionInfoList){
+
+    //卡别
+    var cardZ = document.getElementById('cardZ');
+    var cardZF = document.getElementById('cardZF');
+
+    cardZ.className = "i_nomal";
+    cardZF.className = "i_nomal";
+
+    cardZ.style.display = "none";
+    cardZF.style.display = "none";
+
+    //先初始化
+    var cardP = document.getElementById('cardP');
+    var cardJ = document.getElementById('cardJ');
+    var cardB = document.getElementById('cardB');
+    var cardT = document.getElementById('cardT');
+
+    cardP.className = "i_nomal";
+    cardJ.className = "i_nomal";
+    cardB.className = "i_nomal";
+    cardT.className = "i_nomal";
+
+    var cardLevel ;
+    if(cardClass == "cardP"){
+        cardP.className = "i_nomal i_sel";
+        cardLevel = "P";
+    }else if(cardClass == "cardJ"){
+        cardJ.className = "i_nomal i_sel";
+        cardLevel = "J";
+    }else if(cardClass == "cardB"){
+        cardB.className = "i_nomal i_sel";
+        cardLevel = "B";
+    }else if(cardClass == "cardT"){
+        cardT.className = "i_nomal i_sel";
+        cardLevel = "T";
+    }
+
+    //展示卡别
+
+    for(i=0 ; i < cardVersionInfoList.length ; i++){
+        var kcoll = cardVersionInfoList[i];
+        if( kcoll.cardBrand == cardBrand && kcoll.cardClass == cardLevel){
+            if(kcoll.cardStyle == 'Z'){
+                cardZ.style.display = "";
+            }else if(kcoll.cardStyle == 'F'){
+                cardZF.style.display = "";
+            }
+        }
+    }
+    
+//    if(cardId != "1042"){  
+
+//    }
+
+    return cardLevel;
+}
+
+//卡别选中
+function changeCardStyle(cardStyle){
+
+    var cardZ = document.getElementById('cardZ');
+    var cardZF = document.getElementById('cardZF');
+
+    cardZ.className = "i_nomal";
+    cardZF.className = "i_nomal";
+
+    var style;
+    if(cardStyle == "cardZ"){
+        cardZ.className = "i_nomal i_sel";
+        style = "Z";
+    }else if(cardStyle == "cardZF"){
+        cardZF.className = "i_nomal i_sel";
+        style = "ZF";
+    }
+    return style;
+}
+
+
+//动态加载图片
+function atterImg(cardId , cardVersionInfoList){
+
+    var Oul = document.getElementById('cardCImgList');
+    Oul.style.width = 270*(cardVersionInfoList.length) + 'px';
+
+    for(var i=0;i<cardVersionInfoList.length;i++){
+        var kcoll = cardVersionInfoList[i];
+        var ImgList = document.getElementById('cardCImgList');
+        // var li = new li();
+        var li = document.createElement("li");
+        li.id = kcoll.cardVersionId;
+        var img = document.createElement("img");
+        img.src = "static/images/" + kcoll.pictureName;
+        li.appendChild(img);
+        ImgList.appendChild(li);
+    }
+
+}
+
+
+//清除和选中图片
+function checkedImg(cardId, cardBrand, cardClass, cardStyle, cardVersionInfoList){
+
+    var Oul = document.getElementById('cardCImgList');
+    var Ospan = document.getElementById('huadong');
+
+    var style = cardStyle;
+    if(cardStyle == "B"){
+        style = "Z";
+    }
+
+    if(cardId != "1042"){
+
+        for(var i=0;i<cardVersionInfoList.length;i++){
+            var kcoll = cardVersionInfoList[i];
+    
+            var liId = document.getElementById(kcoll.cardVersionId);
+            liId.className = "";
+    
+        }
+        if(cardClass=="" && cardStyle == ""){
+            return;
+        }
+
+        for(var i=0;i<cardVersionInfoList.length;i++){
+            var kcoll = cardVersionInfoList[i];
+    
+            if(cardBrand == kcoll.cardBrand && cardClass == kcoll.cardClass && style == kcoll.cardStyle ){
+                var liId = document.getElementById(kcoll.cardVersionId);
+                liId.className = "sel";
+                Oul.style.left = '-' + i*202 + 'px';
+                Ospan.style.left = '+' + i*100 + 'px';
+                
+            }
+    
+        }
+    }
+
+}
+
+//one卡图片加载
+function atterImgOne(cardId , cardVersionInfoList , cardVersionClassInfoList){
+
+    var Oul = document.getElementById('cardCImgList');
+    Oul.style.width = 270*(cardVersionClassInfoList.length) + 'px';
+
+    for(var i=0;i<cardVersionClassInfoList.length;i++){
+        var kcoll = cardVersionClassInfoList[i];
+        var ImgList = document.getElementById('cardCImgList');
+        // var li = new li();
+        var li = document.createElement("li");
+        li.id = kcoll.flowNo;
+        
+        var a = document.createElement("a");
+        a.id = kcoll.cardVersion;
+        
+        //one卡选中项
+        a.onclick = function() { selectOneCard( this.id, cardVersionClassInfoList )};
+
+        var img = document.createElement("img");
+        img.src = "static/images/" + kcoll.pictureName;
+
+        a.appendChild(img);
+        li.appendChild(a);
+        ImgList.appendChild(li);
+    }
+
+}
+
+//one卡选中项
+function selectOneCard(cardVersion , cardVersionClassInfoList){
+
+    for(i=0;i<cardVersionClassInfoList.length;i++){
+        var kcoll = cardVersionClassInfoList[i];
+
+        var liId = document.getElementById(kcoll.flowNo);
+        liId.className = "";
+
+        if(cardVersion == kcoll.cardVersion){
+            var liId = document.getElementById(kcoll.flowNo);
+            liId.className = "sel";
+
+            oneCardType = cardVersion;
+        }
+
+    }
+}
+
+
+//外币卡展示项
+function showforeignCard(cardBrand,List){
+
+    var foreignCard = document.getElementById('foreignCard');
+    var rebateCard_S = document.getElementById('rebateCard_S');
+    var rebateCard_G = document.getElementById('rebateCard_G');
+    // var foreignCardImg = document.getElementById('foreignCardImg');
+    
+    var cardBandType;
+    if(cardBrand == "cardCM"){
+        cardBandType = "CM";
+        foreignCard.style.display = "";
+    }else if(cardBrand == "cardCV"){
+        cardBandType = "CV";
+        foreignCard.style.display = "";
+    }else {
+        cardBandType = "";
+        foreignCard.style.display = "none";
+    }
+
+    rebateCard_S.style.display = "none";
+    rebateCard_G.style.display = "none";
+
+    for(i=0; i<List.length; i++){
+        var kcoll = List[i];
+
+        if( cardBandType == "CM"){
+            if(kcoll.cardBrand == "1" && kcoll.cardClass == "1"){
+                rebateCard_G.style.display = "block";
+            }else if(kcoll.cardBrand == "1" && kcoll.cardClass == "2"){
+                rebateCard_S.style.display = "block";
+            }
+        }else if(cardBandType == "CV"){
+            if(kcoll.cardBrand == "2" && kcoll.cardClass == "1"){
+                rebateCard_G.style.display = "block";
+            }else if(kcoll.cardBrand == "2" && kcoll.cardClass == "2"){
+                rebateCard_S.style.display = "block";
+            }
+        }
+    }
+
+    return cardBandType;
+}
+
+
+//显示外币卡图片
+function showforeignCardImg(rebateCard,foreignInternationalCard,List){
+
+    var foreignCardImg = document.getElementById('foreignCardImg');
+    var rebateCardType;
+    var foreignInternationalCardType;
+
+    if(rebateCard == "S"){
+        rebateCardType = "2";
+    }else if(rebateCard == "G"){
+        rebateCardType = "1";
+    }
+
+
+    if(foreignInternationalCard == "CM"){
+        foreignInternationalCardType = "1";
+    }else if(foreignInternationalCard == "CV"){
+        foreignInternationalCardType = "2";
+    }
+
+    for(i=0; i<List.length; i++){
+        var kcoll = List[i];
+        if(kcoll.cardBrand == foreignInternationalCardType && kcoll.cardClass == rebateCardType){
+            foreignCardImg.src = "static/images/" + kcoll.pictureName;
+        }
+    }
+
+    if(rebateCard == "S"){
+        return "S";
+    }else{
+        return "";
+    }
+}
+
+//爱心（希望卡）展示项
+function showHopeCard(cardId){
+
+    var selectHopeCard = document.getElementById('selectHopeCard');
+    var hopeCardNo = document.getElementById('hopeCardNo');
+    var hopeCardMoney = document.getElementById('hopeCardMoney');
+
+    if(cardId == "1001"){
+        selectHopeCard.style.display="";
+        hopeCardNo.innerHTML = "一";
+        hopeCardMoney.innerHTML = "0";
+    }
+
+    //环球悦购卡展示字段
+
+    var huanqiuCard = document.getElementById('huanqiuCard');
+    if(cardId == "1029"){
+        huanqiuCard.style.display="";
+    }
+}
+
+//宠物卡 展示项
+function showpetName(cardId){
+    var petName = document.getElementById('petName');
+
+    if(cardId == "1022"){
+        petName.style.display="";
+    }
+}
+
+
+//粤通卡 展示项
+function showYueTong(cardId){
+    var texingCard = document.getElementById('texingCard');  //车身颜色
+    var carColour = document.getElementById('carColour');  //车身颜色
+    var carSeat = document.getElementById('carSeat');      //车座位数
+    var carModel = document.getElementById('carModel');    //品牌/型号
+
+    if(cardId == "1015"){
+        texingCard.style.display="";
+        carColour.style.display="";
+        carSeat.style.display="";
+        carModel.style.display="";
+    }
+}
+
+//苏通卡 展示项
+function showSuTong(cardId){
+
+    var carColour = document.getElementById('carColour');  //车身颜色
+    var carSeat = document.getElementById('carSeat');      //车座位数
+    var carDiscernNo = document.getElementById('carDiscernNo');  //车辆识别号
+
+    if(cardId == "1023"){
+        carColour.style.display="";
+        carSeat.style.display="";
+        carDiscernNo.style.display="";
+    }
+
+}
